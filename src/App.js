c4583922c133fd1cfe19d25c1f58c7764be0e731
Ben Eccles <ben.g.eccles@icloud.com>
2019-08-26 14:23:54 -0600

    Imported Components into App.js, added component references to return statement in class App.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 import TopicBrowser from './components/TopicBrowser/TopicBrowser';
+import EvenAndOdd from '../Topics/EvenAndOdd';
+import FilterObject from '../Topics/FilterObject';
+import FilterString from '../Topics/FilterString';
+import Palindrome from '../Topic/Palindrome';
+import Sum from '../Topics/Sum';
 
 class App extends Component {
   render() {
     return (
       <TopicBrowser />
+      <EvenAndOdd />
+      <FilterObject />
+      <FilterString />
+      <Palindrome />
+      <Sum />
     )
   }
 }
